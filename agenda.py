6fc760e5fa7cc11e5c323462337074c14802a45c
vladvlad12 <morozanvlad13@gmail.com>
2024-12-02 11:33:20 +0300

    Css actualizat

--- agenda.py
+++ agenda.py
@@ -493,4 +493,4 @@ def sort_recursively(data):
 
 # Rulare aplicație
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=443)
